3082a0d2911363f5b66920e2e601e5363a210b4f
Crowtech <71722272+Crowntec@users.noreply.github.com>
2025-01-06 06:54:52 +0100

    update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash                                                                                                                     # Check if the local repository exists
+                                                                    # Pull the latest changes from the repository
+    git pull && python goldeagle.py
+
+                                                                
+                                                                
+
+                                                                
